73c109b1dc4cc8ed378479d1238589b5c19eba38
omarsilverio <omar.silverio.98@gmail.com>
2025-07-29 23:08:33 -0600

    implementacion de angular

--- src/resources/js/controllers/controllerCatalogo.js
+++ src/resources/js/controllers/controllerCatalogo.js
@@ -4,15 +4,22 @@
 // Este archivo se carga solo cuando se entra a /productos
 
 app.registerController('catalogoController', function($scope) {
+    $scope.filtros = [
+        {descripcion: 'pasteles'},
+        {descripcion: 'gelatinas'},
+        {descripcion: 'panes'},
+        {descripcion: 'postres'}
+    ];
+
     $scope.postres = [
         { nombre_producto:'Delicious Pizza', tipo:'gelatinas', nombre_imagen:'f1.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
-        { nombre_producto:'Delicious Burger', tipo:'pasteles', nombre_imagen:'f2.png', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''},
-        { nombre_producto:'', tipo:'', nombre_imagen:'', descripcion:''}
+        { nombre_producto:'Delicious Burger', tipo:'pasteles', nombre_imagen:'f2.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Delicious Pizza', tipo:'gelatinas', nombre_imagen:'f3.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Delicious Pasta', tipo:'panes', nombre_imagen:'f4.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'French Fries', tipo:'postres', nombre_imagen:'f5.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Delicious Pizza', tipo:'gelatinas', nombre_imagen:'f6.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Tasty Burger', tipo:'pasteles', nombre_imagen:'f7.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Tasty Burger', tipo:'pasteles', nombre_imagen:'f8.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'},
+        { nombre_producto:'Delicious Pasta', tipo:'panes', nombre_imagen:'f9.png', descripcion:'Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque'}
     ];
 });
--- src/views/catalogo.php
+++ src/views/catalogo.php
@@ -8,26 +8,25 @@
 
     <ul class="filters_menu">
       <li class="active" data-filter="*">Todo</li>
-      <li data-filter=".pasteles">Pasteles</li>
-      <li data-filter=".gelatinas">Gelatinas</li>
-      <li data-filter=".panes">Panes</li>
-      <li data-filter=".postres">Postres</li>
+      <li ng-repeat="filtro in filtros" data-filter=".{{filtro.descripcion}}">
+        {{filtro.descripcion}}
+      </li>
     </ul>
 
     <div class="filters-content">
       <div class="row grid">
-        <div class="col-sm-6 col-lg-4 all gelatinas">
+        <div ng-repeat="postre in postres" class="col-sm-6 col-lg-4 all {{ postre.tipo }}">
           <div class="box">
             <div>
               <div class="img-box">
-                <img src="src/resources/images/catalogo/f1.png" alt="">
+                <img src="src/resources/images/catalogo/{{ postre.nombre_imagen }}" alt="">
               </div>
               <div class="detail-box">
                 <h5>
-                  Delicious Pizza
+                  {{ postre.nombre_producto }}
                 </h5>
                 <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
+                  {{ postre.descripcion }}
                 </p>
                 <div class="options">
                   <h6>
@@ -35,213 +34,6 @@
                   </h6>
                   <a>
                     <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6 col-lg-4 all pasteles">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f2.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Delicious Burger
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $15
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="col-sm-6 col-lg-4 all gelatinas">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f3.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Delicious Pizza
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $17
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-lg-4 all panes">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f4.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Delicious Pasta
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $18
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-lg-4 all postres">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f5.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  French Fries
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $10
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>      
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-lg-4 all gelatinas">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f6.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Delicious Pizza
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $15
-                  </h6>
-                  <a>
-                      <i class="fa fa-shopping-cart" aria-hidden="true"></i>      
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-lg-4 all pasteles">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f7.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Tasty Burger
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $12
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>      
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-sm-6 col-lg-4 all burger">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f8.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Tasty Burger
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $14
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>      
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="col-sm-6 col-lg-4 all panes">
-          <div class="box">
-            <div>
-              <div class="img-box">
-                <img src="src/resources/images/catalogo/f9.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Delicious Pasta
-                </h5>
-                <p>
-                  Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque
-                </p>
-                <div class="options">
-                  <h6>
-                    $10
-                  </h6>
-                  <a>
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>      
                   </a>
                 </div>
               </div>
